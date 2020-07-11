3fa948eb7e50dce88dbcd51d3614acb010dc603f
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-07-11 13:01:03 +0300

    Add User resources  - create user migration file  - create user model  - add bcrypt gem  - add hirb gem  - generate users table in the database  - seed users table

--- Gemfile
+++ Gemfile
@@ -24,10 +24,11 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'semantic-ui-sass'
 gem 'jquery-rails'
+gem 'hirb'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'biniam', password:'test')
+User.create(username: 'temesghen', password:'test')
+User.create(username: 'yemane', password:'test')
+User.create(username: 'debas', password:'test')
+User.create(username: 'mulai', password:'test')
