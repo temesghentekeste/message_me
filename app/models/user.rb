a9f3785239ee6530acf746a545f5111fb452aa35
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-07-11 21:54:33 +0300

    Add Messages resources  - add create messages migration  - add Message model and associated validation and association with the user model (belongs_to :user)  - update user model to add the other side of the association (has_many :messages)  - run rails db migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence:true, uniqueness: { case_sensitive: false}, length: { minimum: 3, maximum: 6}
   has_secure_password
+
+  has_many :messages
 end
