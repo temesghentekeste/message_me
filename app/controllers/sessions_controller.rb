95bf67b4a0113e4ba5f7e8df1f2a97f1dd3335a3
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-07-16 22:25:57 +0300

    Add authentication  - Add create session action  - Add destroy action  - Update _navigation partial to address the above changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,23 @@ class SessionsController < ApplicationController
   def new
 
   end
+
+  def create
+    user = User.find_by(username:params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There is something wrong with your login information"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to root_path
+    # byebug
+  end
 end
